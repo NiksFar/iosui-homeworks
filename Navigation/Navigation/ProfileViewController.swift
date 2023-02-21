0aedc9e8a5f634e33ccff7329d0f22d874612d51
Nikita <niksfarisey@yandex.ru>
2023-02-22 01:56:56 +0300

    Initial commit

---
+++ Navigation/Navigation/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Никита on 17.02.2023.
+//
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
