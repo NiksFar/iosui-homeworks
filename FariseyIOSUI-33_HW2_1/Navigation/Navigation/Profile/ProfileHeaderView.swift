dacb5d0d8c8a75ac49727c2f7cd51d57d1dc63ee
Nikita <niksfarisey@yandex.ru>
2023-05-03 21:56:26 +0200

    HW-Done

---
+++ FariseyIOSUI-33_HW2_1/Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,158 @@
+import UIKit
+
+class ProfileHeaderView: UIView {
+    
+    
+    let carImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(named: "Image1")
+        imageView.clipsToBounds = true // Чтобы изображение не выходило за пределы круга
+        imageView.contentMode = .scaleAspectFill // Ресайз контента под фрейм
+        imageView.layer.borderWidth = 3 // Ширина рамки 3пт
+        imageView.layer.borderColor = UIColor.white.cgColor // Задаем цвет рамки
+        imageView.layer.cornerRadius = 75 // Height/2
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    //////////////
+    let textField: UITextField = {
+        
+        var field = UITextField()
+        
+        field.layer.cornerRadius = 12
+        field.backgroundColor = .white
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.black.cgColor
+        
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: field.frame.height)) // Добавляем маленькую вьюшку(белый прямоугольник) - для отступа от края textField
+        field.leftViewMode = .always
+        
+        field.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
+        
+    }()
+    
+    @objc func statusTextChanged(_ textField: UITextField) {
+        //print(#function)
+        statusText = textField.text
+    }
+    
+    private var statusText: String?
+    /////////////
+    
+    let statusButton: UIButton = {
+        
+        let button = UIButton()
+        
+        button.backgroundColor = .blue
+        button.setTitle("Set status", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 16 // Поправить до нужного радиуса
+        button.layer.masksToBounds = false // Обязательно, если хотим видеть тень
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.opacity = 0.7
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        //button.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // Обработка нажатия кнопки
+    @objc func buttonPressed() {
+        statusLabel.text = statusText
+        //print(statusLabel.text ?? "No status")
+    }
+    
+    ////////////
+    
+    let profileLabel: UILabel = {
+        
+        let label = UILabel()
+        
+        label.text = "Hipster Car"
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let statusLabel: UILabel = {
+        
+        let secondaryLabel = UILabel()
+        
+        secondaryLabel.text = "Waiting for a ride"
+        secondaryLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        secondaryLabel.textColor = .gray
+        secondaryLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return secondaryLabel
+    }()
+    
+    private func setupAddsubviews() {
+        self.addSubview(carImageView)
+        self.addSubview(profileLabel)
+        self.addSubview(statusLabel)
+        self.addSubview(textField)
+        self.addSubview(statusButton)
+    }
+    
+    private func setupConstraints() {
+       
+        NSLayoutConstraint.activate([
+        
+            // Подгоняем картинку
+            carImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            carImageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            carImageView.heightAnchor.constraint(equalToConstant: 150),
+            carImageView.widthAnchor.constraint(equalToConstant: 150),
+            
+            // Подгоняем profileLabel
+            profileLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
+            profileLabel.leadingAnchor.constraint(equalTo: carImageView.trailingAnchor, constant: 15),
+            profileLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -15),
+            
+            // Подгоняем statusLabel
+            statusLabel.topAnchor.constraint(equalTo: profileLabel.bottomAnchor, constant: 20),
+            statusLabel.leadingAnchor.constraint(equalTo: carImageView.trailingAnchor, constant: 15),
+            statusLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -15),
+            
+            // Подгоняем кнопку
+            statusButton.topAnchor.constraint(equalTo: carImageView.bottomAnchor, constant: 16),
+            statusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            statusButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            
+            textField.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -15),
+            textField.leadingAnchor.constraint(equalTo: carImageView.trailingAnchor, constant: 15),
+            textField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -15),
+            textField.heightAnchor.constraint(equalToConstant: 40)
+            
+        ])
+        
+    }
+    /*
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print(#function)
+        
+    }
+    */
+    
+    override init (frame: CGRect) { // Прочитать про метод
+        super.init(frame: frame)
+        self.setupAddsubviews()
+        self.setupConstraints()
+        // print(#function)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
